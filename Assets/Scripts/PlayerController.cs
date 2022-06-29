4e16e2943218545941ddadaf9312dac86dc8ec71
TsuyoshiUsugi <qesulive@gmail.com>
2022-06-30 00:19:40 +0900

    ステージを整えた。

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    [SerializeField] Rigidbody _rb;
     [SerializeField] float _walkSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+
+        PlayerRotate();
+    }
+
+    void Move()
+    {
+       
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        _rb.velocity = new Vector3(x * _walkSpeed, 0, z * _walkSpeed);
         
     }
+
+    void PlayerRotate()
+    {
+
+    }
+
 }
