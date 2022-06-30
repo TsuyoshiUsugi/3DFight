3d2cca75f03252e637ade69ab12126ecf6a39681
TsuyoshiUsugi <qesulive@gmail.com>
2022-06-30 15:03:37 +0900

    プレイヤースクリプトの修正

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] Rigidbody _rb;
     [SerializeField] float _walkSpeed;
     [SerializeField] float _cameraSpeed;
+    [SerializeField] float _jumpForce;
+    [SerializeField] float _jumpCount;
+    [SerializeField] float _jumpLimit;
+    [SerializeField] float _gravity;
 
     float _horizontal;
     float _vertical;
@@ -16,18 +20,22 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        //Physics.gravity = new Vector3(0, _gravity, 0);
         _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //WASDのキーを読み取る
         _horizontal = Input.GetAxisRaw("Horizontal");
         _vertical = Input.GetAxisRaw("Vertical");
 
+        //マウスの位置を読み取る
         mouseInputX = Input.GetAxis("Mouse X");
         mouseInputY = Input.GetAxis("Mouse Y");
+
+        Jump();
     }
 
     private void FixedUpdate()
@@ -35,25 +43,61 @@ public class PlayerController : MonoBehaviour
         Move();
 
         PlayerRotate();
+
     }
 
+    /// <summary>
+    /// プレイヤーの移動の関数
+    /// </summary>
     void Move()
     {
+        //カメラの向き
         Vector3 cameraForward = Vector3.Scale(transform.forward, new Vector3(1, 0, 1)).normalized;
 
+        //プレイヤーの進行方向
         Vector3 moveForward = cameraForward * _vertical + transform.right * _horizontal;
-        
-        _rb.velocity = moveForward * _walkSpeed + new Vector3(_horizontal, _rb.velocity.y, _vertical).normalized;
+
+        //カメラの向いてる方にプレイヤーを動かす
+        _rb.velocity = new Vector3(moveForward.x * _walkSpeed, _rb.velocity.y, moveForward.z * _walkSpeed);
         
         
     }
 
+    /// <summary>
+    /// プレイヤーの向きの関数
+    /// </summary>
     void PlayerRotate()
     {
         transform.rotation = Quaternion.Euler(transform.eulerAngles.x,
             transform.eulerAngles.y + mouseInputX * _cameraSpeed,
             transform.eulerAngles.z);
+    }
+
+    /// <summary>
+    /// ジャンプの関数
+    /// </summary>
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && _jumpCount < _jumpLimit)
+        {
+
+            _rb.AddForce(transform.up * _jumpForce);
+            _jumpCount++;
+        }
 
         
+    }
+
+    /// <summary>
+    /// 着地判定の関数
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground") == true)
+        {
+            _jumpCount = 0;
+        }
+        _jumpCount = 0;
     }
 }
