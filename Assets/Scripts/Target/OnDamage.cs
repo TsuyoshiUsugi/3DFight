41359e47d958955fe003e4940b45bcd28e380fa0
TsuyoshiUsugi <qesulive@gmail.com>
2022-07-13 12:57:54 +0900

    カメラほぼ完成。的完成

--- Assets/Scripts/Target/OnDamage.cs
+++ Assets/Scripts/Target/OnDamage.cs
@@ -11,18 +11,41 @@ public abstract class OnDamage : MonoBehaviour
     ///ダメージを受け取ってplayerコントローラーに伝える
     [SerializeField] Target Player;
 
-    [SerializeField] float _magni;
-    public float Magni { get => _magni; set => _magni = value; }
+    /// <summary>どれだけの間色を変えるか</summary>
+    [SerializeField] float _colorTimer = 1f;
 
+    /// <summary>当たった時の色</summary>
+    [SerializeField] Material _hitMaterial;
+
+    /// <summary>元の色</summary>
+    [SerializeField] Material _originMaterial;
+
+    /// <summary>被弾時の倍率</summary>
+    [SerializeField] float _magni;
+
+    /// <summary>受け取った弾丸のダメージを入れる変数</summary>
     [SerializeField] float _damage;
 
+    /// <summary>
+    /// 倍率のプロパティ
+    /// </summary>
+    public float Magni { get => _magni; set => _magni = value; }
+
+
+    /// <summary>
+    /// ダメージのプロパティ
+    /// </summary>
     public float Damage { get => _damage; set => _damage = value; }
 
     private void Start()
     {
+        _originMaterial = GetComponent<Renderer>().material;
         Player = GetComponentInParent<Target>();
     }
 
+    /// <summary>
+    /// 弾丸から受け取ったダメージに倍率を掛けて返す
+    /// </summary>
     public void DamagePoint()
     {
         if (_damage != 0)
@@ -30,5 +53,17 @@ public abstract class OnDamage : MonoBehaviour
             Player.Hp -= _damage * _magni;
             _damage = 0;
         }
+    }
+
+    protected void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine("HitColorChange");
+    }
+
+    IEnumerator HitColorChange()
+    {
+        gameObject.GetComponent<Renderer>().material = _hitMaterial;
+        yield return new WaitForSeconds(_colorTimer);
+        gameObject.GetComponent<Renderer>().material = _originMaterial;
     }
 }
