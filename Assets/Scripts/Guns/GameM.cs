94026c3474a94a7162bf518505b301b64d5c1d53
TsuyoshiUsugi <qesulive@gmail.com>
2022-07-18 18:03:18 +0900

    必要になるであろうシーンと仮の遷移スクリプト作成

--- Assets/Scripts/Guns/GameM.cs
+++ Assets/Scripts/Guns/GameM.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 /// <summary>
 /// ゲーム全体の管理を行うコンポーネント
+/// 機能
+/// カーソルを消す
+/// 
 /// </summary>
 public class GameM : MonoBehaviour
 {
@@ -21,11 +24,21 @@ public class GameM : MonoBehaviour
 
     /// <summary>
     /// マウスカーソルを中央に固定し、見えなくさせる
+    /// Escをおしたら見えるようにする
     /// </summary>
     void CursorSet()
     {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+
     }
 
 }
