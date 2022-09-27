96202e2a7f057c4e362aabb3ca52fa92ad34fe12
TsuyoshiUsugi <qesulive@gmail.com>
2022-09-28 02:29:44 +0900

    [add] singletonの基底クラスを追加

---
+++ Assets/Scripts/Singleton/SingletonMonoBehavior.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using System;
+
+/// <summary>
+/// シングルトンモノビヘイビアクラス
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public abstract class SingletonMonoBehavior<T>: MonoBehaviour where T : MonoBehaviour
+{
+    protected static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            //インスタンスがないなら探す
+            if (_instance == null)
+            {
+                Type t = typeof(T);
+
+                _instance = (T)FindObjectOfType(t);
+
+                //見つからない場合エラーをログに表示する
+                if(_instance == null)
+                {
+                    Debug.LogError($"{t} のインスタンスがありません");
+                }
+
+            }
+            return _instance;
+        }
+    }
+
+    virtual protected void Awake()
+    {
+        CheckInstance();
+    }
+
+    protected bool CheckInstance()
+    {
+        if(_instance == null)
+        {
+            _instance = this as T;
+            return true;
+        }
+        else if(Instance == this)
+        {
+            return true;
+        }
+
+        Destroy(this);
+        return false;
+    }
+}
